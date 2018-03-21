944c6f01ea4229a8faad23c22aedd070fa84b5d9
Inhere <inhere@users.noreply.github.com>
2018-03-21 18:22:52 +0800

    Update ViewCollector.php

--- src/Bean/Collector/ViewCollector.php
+++ src/Bean/Collector/ViewCollector.php
@@ -30,7 +30,7 @@ class ViewCollector implements CollectorInterface
      * @param null   $propertyValue
      * @throws \InvalidArgumentException
      */
-    public static function collect(string $className, $objectAnnotation = null, string $propertyName = "", string $methodName = "", $propertyValue = null)
+    public static function collect(string $className, $objectAnnotation = null, string $propertyName = '', string $methodName = '', $propertyValue = null)
     {
         if ($objectAnnotation instanceof View) {
             self::$views[$className]['view'][$methodName] = [
