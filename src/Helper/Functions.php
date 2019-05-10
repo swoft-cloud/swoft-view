5181c56ccb653251246a926836bf6e4edd43f36c
inhere <in.798@qq.com>
2019-05-11 01:14:54 +0800

    update some

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -1,23 +1,27 @@
 <?php declare(strict_types=1);
 
+use Swoft\Context\Context;
+use Swoft\Http\Message\Response;
+use Swoft\View\Renderer;
+
 if (!function_exists('view')) {
     /**
      * @param string            $template
      * @param array             $data
      * @param string|null|false $layout
      *
-     * @return \Swoft\Http\Message\Response
+     * @return Response
      * @throws Throwable
      */
     function view(string $template, array $data = [], $layout = null)
     {
         /**
-         * @var \Swoft\View\Renderer         $renderer
-         * @var \Swoft\Http\Message\Response $response
+         * @var Renderer $renderer
+         * @var Response $response
          */
-        $renderer = \Swoft::getSingleton('view');
-        $response = \Swoft\Context\Context::mustGet()->getResponse();
-        $content  = $renderer->render(\Swoft::getAlias($template), $data, $layout);
+        $renderer = Swoft::getSingleton('view');
+        $response = Context::mustGet()->getResponse();
+        $content  = $renderer->render(Swoft::getAlias($template), $data, $layout);
 
         return $response
             ->withContent($content)
